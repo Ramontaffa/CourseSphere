cfa503c85df1fb3fff0cc5cc6c9f860e02360610
Ramontaffa <taffarelramon1@gmail.com>
2025-10-21 15:47:18 -0300

    feat(auth): replace login page content with LoginComponent

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,10 +1,7 @@
+import { LoginComponent } from "@/components/auth/loginForm";
+
 export default function Page() {
   return (
-    <div className="flex flex-col h-screen justify-between items-center p-24">
-      <h1 className="text-4xl font-bold">Login Page</h1>
-      <p className="mt-4 text-lg text-gray-600">
-        Por favor, faça login para continuar.
-      </p>
-    </div>
+    <LoginComponent />
   );
 }
