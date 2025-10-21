fec041328df1850018bb12d09e4fb696fb0a632b
Ramontaffa <taffarelramon1@gmail.com>
2025-10-21 15:47:24 -0300

    fix(layout): improve layout styling for better content alignment and spacing

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -4,11 +4,11 @@ import { SplashImage } from "@/assets";
 export default function authLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col md:flex-row">
-      <main className="w-full md:w-3/8 bg-gray-50 dark:bg-gray-900">
-          {children}
+      <main className="w-full flex items-center justify-center min-h-screen md:w-3/8 p-8 bg-off-white dark:bg-gray-900">
+        {children}
       </main>
 
-      <aside className="md:w-5/8 bg-gray-100 dark:bg-gray-800 flex items-center justify-center">
+      <aside className="md:w-5/8 bg-gray-100 dark:bg-gray-800">
         <Image
           src={SplashImage}
           alt="CourseSphere Splash Image"
