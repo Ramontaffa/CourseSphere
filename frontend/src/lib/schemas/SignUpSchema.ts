a71af08732c3d8e1fcfc2bc0eca8d370654c1d22
Ramontaffa <taffarelramon1@gmail.com>
2025-10-21 21:17:55 -0300

    refactor(schemas): restrutured schemas folders

--- frontend/src/lib/schemas/SignUpSchema.ts
+++ frontend/src/lib/schemas/SignUpSchema.ts
@@ -1,18 +1,5 @@
 import { z } from "zod";
 
-export const loginSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: "O email é obrigatório." })
-    .email({ message: "Por favor, insira um email válido." }),
-
-  password: z
-    .string()
-    .min(6, { message: "A senha deve ter no mínimo 6 caracteres." }),
-});
-
-export type LoginFormData = z.infer<typeof loginSchema>;
-
 export const signUpSchema = z
   .object({
     name: z
