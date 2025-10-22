f8c44df3b15a0a125e08372a5ba574200fb59e24
Ramontaffa <taffarelramon1@gmail.com>
2025-10-21 21:08:51 -0300

    feat(hooks): add useCourses hook for fetching user-specific courses

---
+++ frontend/src/hooks/useCourses.ts
@@ -0,0 +1,44 @@
+"use client";
+
+import useSWR from "swr";
+import { useAuth } from "@/context/AuthContext";
+import { useMemo } from "react";
+import { Course } from "@/types";
+
+export function useCourses() {
+  const { user, isHydrating } = useAuth();
+
+  const shouldFetch = !isHydrating && user;
+
+  const {
+    data: allCourses,
+    error,
+    isLoading: isSWRLoading,
+  } = useSWR<Course[]>(shouldFetch ? "/courses" : null);
+
+  console.log("All Courses:", allCourses);
+  console.log("User:", user);
+  console.log("isHydrating:", isHydrating);
+  console.log("swr: ", { isSWRLoading, error });
+
+  const courses = useMemo(() => {
+    if (!allCourses || !user) return [];
+
+    const userIdAsString = String(user.id);
+
+    return allCourses.filter((course) => {
+      const isCreator = String(course.creator_id) === userIdAsString;
+      const isInstructor = Array.isArray(course.instructors) && course.instructors.map(String).includes(userIdAsString);
+
+      return isCreator || isInstructor;
+    });
+  }, [allCourses, user]);
+
+  const isLoading = isHydrating || isSWRLoading;
+
+  return {
+    courses,
+    error,
+    isLoading,
+  };
+}
