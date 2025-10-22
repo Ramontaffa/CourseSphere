ffdca2f39945844d34e45a75ca6bbd3b49b3a684
Ramontaffa <taffarelramon1@gmail.com>
2025-10-21 21:00:48 -0300

    feat(types): include Course type in exported types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,4 +1,5 @@
 import { User } from './user';
+import { Course } from './coursesType';
 import { AuthContextType, LoginCredentials, SignUpCredentials } from './authTypes';
 
-export type { User, AuthContextType, LoginCredentials, SignUpCredentials };
+export type { User, Course, AuthContextType, LoginCredentials, SignUpCredentials };
