5c855438d447c495aa9068feb3ce585bad6fa535
Ramontaffa <taffarelramon1@gmail.com>
2025-10-22 23:42:52 -0300

    feat(CourseCard): create CourseCard component to display course details with date formatting

---
+++ frontend/src/components/molecules/CourseCard/CourseCard.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Course } from "@/types";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@atoms/Card/card";
+import { Calendar } from "lucide-react";
+import Link from "next/link";
+
+type CourseCardProps = {
+  course: Course;
+};
+
+export function CourseCard({ course }: CourseCardProps) {
+  function formatDate(dateInput: string | Date) {
+    if (!dateInput) return "";
+    const date = typeof dateInput === "string" ? new Date(dateInput) : dateInput;
+    if (isNaN(date.getTime())) return "Data inválida";
+    return date.toLocaleDateString("pt-BR");
+  }
+
+  console.log("ID DO CURSO NO CARD:", course.id)
+  return (
+    <Link
+      className="group block w-full max-w-[45%] h-full mx-auto transition-transform transform hover:scale-103 focus:scale-103"
+      href={`/course/${course.id}`}
+    >
+      <Card className="flex h-full flex-col cursor-pointer overflow-hidden transition-shadow duration-300 border-border/50 hover:shadow-md">
+        <CardHeader className="grow">
+          <h3 className="text-xl font-semibold text-main-blue line-clamp-2 group-hover:text-main-blue-hover group-focus:text-primary transition-colors duration-300">
+            {course.name}
+          </h3>
+
+          <CardDescription className="text-sm text-muted-foreground mt-2 line-clamp-2 h-10">
+            {course.description || "Este curso não tem descrição."}
+          </CardDescription>
+        </CardHeader>
+
+        <CardFooter className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-4 text-sm text-muted-foreground p-4 border-t border-border/50">
+          <div className="flex items-center gap-1.5">
+            <Calendar className="w-4 h-4 text-main-dark-blue" />
+            <span>{formatDate(course.start_date)}</span>
+          </div>
+          <span className="hidden md:inline">→</span>
+          <div className="flex items-center gap-1.5">
+            <Calendar className="w-4 h-4 text-main-dark-blue" />
+            <span>{formatDate(course.end_date)}</span>
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
